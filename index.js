8cfd02710f3851da07e9af30be1a431e0874a97b
Didier Catz <didierlennon@gmail.com>
2020-05-27 18:17:16 +0200

    Define routes in routes.js

--- index.js
+++ index.js
@@ -5,19 +5,21 @@
  */
 
 // Packages
-const fs = require('fs')
-const express = require('express')
-const app = express()
-const hbs = require('express-handlebars')
+const express = require('express'),
+	app = express(),
+	hbs = require('express-handlebars')
+
 require('dotenv').config()
 
-const { DB_URI, DB_NAME } = process.env
+const { DB_URI, DB_NAME, PORT } = process.env
 
 const uri = DB_URI
-
 const MongoClient = require('mongodb').MongoClient
 const client = new MongoClient(uri, { useUnifiedTopology: true })
 
+/**
+ * @param users - A list of users in the database
+ */
 let users = []
 client.connect((err, client) => {
 	if (err) {
@@ -28,12 +30,11 @@ client.connect((err, client) => {
 		.db(DB_NAME)
 		.collection('users')
 		.find()
-		.limit(1)
+		.limit(100)
 		.toArray((err, docs) => {
 			if (err) {
 				throw err
 			}
-			// console.log(docs)
 			users = docs
 			client.close()
 		})
@@ -44,7 +45,7 @@ client.connect((err, client) => {
  * Default is `process.env.PORT`. Otherwise, it falls back to port 3000.
  * @constant
  */
-const port = process.env.PORT || 3000
+const port = PORT || 3000
 
 app
 	// Serve static files in `/public`
@@ -72,17 +73,13 @@ app
 
 // Application running on port...
 const server = app.listen(port, () => {
-	console.log(`Miit is running in ${process.env.NODE_ENV} mode on http://localhost:${port + 1}`)
-	process.send && process.send('online')
+	console.log(`Miit is running in ${process.env.NODE_ENV} mode on http://localhost:${port}`)
 })
 
 const io = require('socket.io')(server)
 
 /** Defines the routes that will be served up by the server. */
-const routes = {
-	'/': 'index.hbs',
-	'*': '404.hbs',
-}
+const routes = require('./src/routes')
 
 // Loop over and destructure the routes object, keepin' it DRY
 for (const [route, source] of Object.entries(routes)) {
